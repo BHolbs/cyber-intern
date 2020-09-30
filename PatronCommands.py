72bdb30372d791d8edbe168696a13d155bc916e0
BHolbs <blaineholbert@gmail.com>
2020-09-30 00:49:49 -0700

    Need to access 0th element

--- PatronCommands.py
+++ PatronCommands.py
@@ -32,7 +32,7 @@ class PatronCommands(commands.Cog):
                     if len(time) == 0:
                         self.when_blue_last_mentioned = None
                     else:
-                        self.when_blue_last_mentioned = datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
+                        self.when_blue_last_mentioned = datetime.strptime(time[0], "%Y-%m-%d %H:%M:%S.%f")
             except IOError:
                 # something would have to be VERY wrong for this to happen, print to the console and then quit
                 # since this can only happen on startup, quit out if this happens
